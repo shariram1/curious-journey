790f05e8b95859423e7aab169f51932ae848661b
Sankarlal Hariram <mhsankarlal@gmail.com>
2018-10-28 18:59:59 +0800

    Code commit to trigger travis CI

--- sandbox/sandbox/transform.py
+++ sandbox/sandbox/transform.py
@@ -68,7 +68,7 @@ john_data = {
     "street": "123 Main St.",
     "city": "Anytown",
     "state": "FL",
-    "zip": 99999,
+    "zip": 88888,
     "relationships": {
         "siblings": ["Michael R. Public", "Suzy Q. Public"],
         "parents": ["John Q. Public Sr.", "Mary S. Public"],
